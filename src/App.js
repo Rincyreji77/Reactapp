ac8e0bf62127e57bd05017b3ce9af3a0668d3c71
rincyreji <rincy667@gmail.com>
2022-05-17 10:30:55 +0530

    message

--- src/App.js
+++ src/App.js
@@ -2,50 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Addstudent from './component/Addstudent';
+import Searchstudent from './component/Searchstudent';
 
 function App() {
   return (
    
-<div className="container">
-        <div className="row">
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <div className="row g-2">
-                    <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
-                        <label for="" className="form-label">Name</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
-                        <label for="" className="form-label">Roll Number</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
-                        <label for="" className="form-label">Admission Number</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
-                        <label for="" className="form-label">Mobile</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="col col-12 col-sm-12 col-md-12 col lg-12 col-xl-12 col-xxl-12">
-                        <label for="" className="form-label">College</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
-                        <label for="" className="form-label">User Name</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
-                        <label for="" className="form-label">Password</label>
-                        <input type="text" className="form-control"/>
-                    </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col lg-6 col-xl-6 col-xxl-6">
-                       <button className="btn btn-success">Register</button>
-                    </div>
-                  
-                </div>
-            </div>
-        </div>
-    </div>
+<div>
+    <Addstudent/>
+    <Searchstudent/>
+</div>
+      
   );
 }
 
